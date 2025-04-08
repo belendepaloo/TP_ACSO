f6e1d5776bca98020e5159211e4b049c206f5506
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-04-08 20:31:32 -0300

    ej1 funciones c

--- TP2-x86_64/ej1/ej1.c
+++ TP2-x86_64/ej1/ej1.c
@@ -1,15 +1,66 @@
 #include "ej1.h"
 
 string_proc_list* string_proc_list_create(void){
+    string_proc_list* list = (string_proc_list*) malloc(sizeof(string_proc_list));
+    if (!list) {
+        fprintf(stderr, "Error allocating memory for the list\n");
+        return NULL;
+    }
+    list->first = NULL;
+    list->last = NULL;
+    return list;
 }
 
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
+    string_proc_node* node = (string_proc_node*) malloc(sizeof(string_proc_node));
+    if (!node) {
+        fprintf(stderr, "Error allocating memory for the node\n");
+        return NULL;
+    }
+    node->type = type;
+    node->hash = hash;
+    node->next = NULL;
+    node->previous = NULL;
+    return node;
 }
 
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash){
+    string_proc_node* new_node = string_proc_node_create(type, hash);
+    if (!new_node) {
+        fprintf(stderr, "Error creating node\n");
+        return;
+    }
+    if (!(list->first)) {
+        list->first = new_node;
+        list->last = new_node;
+    } else {
+        list->last->next = new_node;
+        new_node->previous = list->last;
+        list->last = new_node;
+    }
 }
 
 char* string_proc_list_concat(string_proc_list* list, uint8_t type , char* hash){
+    char* concat = strdup(hash);
+    if (!concat) {
+        fprintf(stderr, "Error allocating memory for the result\n");
+        return NULL;
+    }
+    string_proc_node* current_node = list->first;
+    while (current_node) {
+        if (current_node->type == type) {
+            char* new_result = realloc(concat, strlen(concat) + strlen(current_node->hash) + 1);
+            if (!new_result) {
+                fprintf(stderr, "Error reallocating memory for the result\n");
+                free(concat);
+                return NULL;
+            }
+            concat = new_result;
+            strcat(concat, current_node->hash);
+        }
+        current_node = current_node->next;
+    }
+    return concat;
 }
 
 
