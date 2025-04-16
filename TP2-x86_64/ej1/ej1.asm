c383be58e90574dca0a240041b8fa936c56dd39a
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-04-16 12:19:18 -0300

    Update ej1.asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,3 +1,10 @@
+; /** defines bool y puntero **/
+%define NULL 0
+%define TRUE 1
+%define FALSE 0
+
+section .data
+
 section .text
 
 global string_proc_list_create_asm
@@ -5,10 +12,10 @@ global string_proc_node_create_asm
 global string_proc_list_add_node_asm
 global string_proc_list_concat_asm
 
+; FUNCIONES auxiliares que pueden llegar a necesitar:
 extern malloc
 extern free
 extern str_concat
-extern strlen
 extern strcpy
 
 string_proc_list_create_asm:
