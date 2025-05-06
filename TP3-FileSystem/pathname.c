04f4c9e5f253bf279672e10a7e579aacc5b25e31
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-05-06 00:37:16 -0300

    Update pathname.c

--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -11,6 +11,27 @@
  * TODO
  */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    //Implement code here
-	return 0;
+    if (pathname == NULL || pathname[0] != '/') {
+        return -1; // Ruta inválida
+    }
+
+    int inumber = 1; // Comenzar desde el inodo raíz
+
+    // Crear una copia mutable del path
+    char path_copy[strlen(pathname) + 1];
+    strcpy(path_copy, pathname);
+
+    // Tokenizar el path por '/'
+    char *token = strtok(path_copy, "/");
+    while (token != NULL) {
+        struct direntv6 dirEnt;
+        if (directory_findname(fs, token, inumber, &dirEnt) < 0) {
+            return -1; // Componente no encontrado
+        }
+        inumber = dirEnt.d_inumber;
+        token = strtok(NULL, "/");
+    }
+
+    return inumber;
 }
+
