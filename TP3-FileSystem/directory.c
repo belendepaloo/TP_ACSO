64d7819b9b00d5c1ed965217c286af97893b83f7
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-05-06 00:34:49 -0300

    Update directory.c

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -10,7 +10,36 @@
  * TODO
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
-		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+                       int dirinumber, struct direntv6 *dirEnt) {
+    struct inode in;
+    if (inode_iget(fs, dirinumber, &in) < 0) {
+        return -1;
+    }
+
+    if (!(in.i_mode & IALLOC) || ((in.i_mode & IFMT) != IFDIR)) {
+        // No está asignado o no es un directorio
+        return -1;
+    }
+
+    int filesize = inode_getsize(&in);
+    int num_blocks = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
+
+    for (int bno = 0; bno < num_blocks; bno++) {
+        char buf[DISKIMG_SECTOR_SIZE];
+        int bytes = file_getblock(fs, dirinumber, bno, buf);
+        if (bytes < 0) return -1;
+
+        int nentries = bytes / sizeof(struct direntv6);
+        struct direntv6 *entries = (struct direntv6 *) buf;
+
+        for (int i = 0; i < nentries; i++) {
+            if (strncmp(name, entries[i].d_name, 14) == 0) {
+                *dirEnt = entries[i];
+                return 0;
+            }
+        }
+    }
+
+    return -1;  // No se encontró
 }
+
