fd1bcb2497500a4821c3813c058b3b500e7b1035
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-05-06 00:31:06 -0300

    Update file.c

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -9,7 +9,29 @@
  * TODO
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-    
-    return 0;
+    struct inode in;
+    if (inode_iget(fs, inumber, &in) < 0) {
+        return -1;
+    }
+
+    int block = inode_indexlookup(fs, &in, blockNum);
+    if (block == -1) {
+        return -1;
+    }
+
+    int bytes = diskimg_readsector(fs->dfd, block, buf);
+    if (bytes == -1) {
+        return -1;
+    }
+
+    int filesize = inode_getsize(&in);
+    int max_bytes = filesize - (blockNum * DISKIMG_SECTOR_SIZE);
+
+    if (max_bytes >= DISKIMG_SECTOR_SIZE) {
+        return DISKIMG_SECTOR_SIZE;
+    } else {
+        return max_bytes > 0 ? max_bytes : 0;
+    }
 }
+
 
