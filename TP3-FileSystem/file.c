2d358f2cde35074003c140e4f545f4b56eaa3b34
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-05-14 14:35:51 -0300

    Update file.c

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -5,29 +5,31 @@
 #include "inode.h"
 #include "diskimg.h"
 
-int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
+int file_getblock(struct unixfilesystem *file_system, int inumber, int blockNum, void *buffer) {
     struct inode inode_data;
-    if (inode_iget(fs, inumber, &inode_data) < 0) {
+    if (inode_iget(file_system, inumber, &inode_data) < 0) {
         return -1;
     }
 
-    int block = inode_indexlookup(fs, &inode_data, blockNum);
+    int block = inode_indexlookup(file_system, &inode_data, blockNum);
     if (block == -1) {
         return -1;
     }
 
-    int bytes = diskimg_readsector(fs->dfd, block, buf);
+    int bytes = diskimg_readsector(file_system->dfd, block, buffer);
     if (bytes == -1) {
         return -1;
     }
 
-    int filesize = inode_getsize(&inode_data);
-    int max_bytes = filesize - (blockNum * DISKIMG_SECTOR_SIZE);
+    int file_size = inode_getsize(&inode_data);
+    int remaining_bytes = file_size - (blockNum * DISKIMG_SECTOR_SIZE);
 
-    if (max_bytes >= DISKIMG_SECTOR_SIZE) {
+    if (remaining_bytes <= 0) {
+        return 0;
+    } else if (remaining_bytes >= DISKIMG_SECTOR_SIZE) {
         return DISKIMG_SECTOR_SIZE;
     } else {
-        return max_bytes > 0 ? max_bytes : 0;
+        return remaining_bytes;
     }
 }
 
