7c0b31f60d5ca872e05d56fc4bd80c67ecee4eef
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-05-14 14:26:24 -0300

    Update directory.c

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -6,30 +6,33 @@
 #include <string.h>
 #include <assert.h>
 
-int directory_findname(struct unixfilesystem *fs, const char *name, int dirinumber, struct direntv6 *dirEnt) {
-    struct inode in;
-    if (inode_iget(fs, dirinumber, &in) < 0) {
+int directory_findname(struct unixfilesystem *file_system, const char *name, int directory_inumber, struct direntv6 *directory_entry) {
+    struct inode inode_data;
+    if (inode_iget(file_system, directory_inumber, &inode_data) < 0) {
         return -1;
     }
 
-    if (!(in.i_mode & IALLOC) || ((in.i_mode & IFMT) != IFDIR)) {
+    if (!(inode_data.i_mode & IALLOC) || ((inode_data.i_mode & IFMT) != IFDIR)) {
         return -1;
     }
 
-    int filesize = inode_getsize(&in);
+    int filesize = inode_getsize(&inode_data);
     int num_blocks = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
+    int block = 0; 
+    for (block; block < num_blocks; block++) {
+        char buffer[DISKIMG_SECTOR_SIZE];
+        int bytes = file_getblock(file_system, directory_inumber, block, buffer);
+        if (bytes < 0){ 
+            return -1;
+        }
 
-    for (int bno = 0; bno < num_blocks; bno++) {
-        char buf[DISKIMG_SECTOR_SIZE];
-        int bytes = file_getblock(fs, dirinumber, bno, buf);
-        if (bytes < 0) return -1;
+        int num_entries = bytes / sizeof(struct direntv6);
+        struct direntv6 *entries = (struct direntv6 *) buffer;
 
-        int nentries = bytes / sizeof(struct direntv6);
-        struct direntv6 *entries = (struct direntv6 *) buf;
-
-        for (int i = 0; i < nentries; i++) {
+        int i = 0;
+        for (i; i < num_entries; i++) {
             if (strncmp(name, entries[i].d_name, 14) == 0) {
-                *dirEnt = entries[i];
+                *directory_entry = entries[i];
                 return 0;
             }
         }
