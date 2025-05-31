c9fd3ff505dad99f6ba08a3da57e4002e2c917e1
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-05-31 13:43:50 -0300

    Update shell.c

--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -25,6 +25,9 @@ int main() {
         if (fgets(command, sizeof(command), stdin) == NULL) {
             break; // EOF (Ctrl+D)
         }
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
 
         // Eliminar el salto de línea al final
         command[strcspn(command, "\n")] = '\0';
