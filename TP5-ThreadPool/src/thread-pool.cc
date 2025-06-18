ea55ac08a3466a80a58848908a391ac4cc48ebf6
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-06-17 23:38:20 -0300

    arreglando

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -6,6 +6,7 @@
 
 #include "thread-pool.h"
 using namespace std;
+#include <iostream>
 
 ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {
     for (size_t i = 0; i < numThreads; ++i) {
@@ -21,11 +22,13 @@ ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {
 
 void ThreadPool::worker(int id) {
     while (true) {
+        cout << "Worker " << id << " arrancó y está esperando trabajo." << endl;
         wts[id].ready.wait();
         if (done) break;
         {
             lock_guard<mutex> lg(wts[id].mtx);
             if (wts[id].thunk) {
+                cout << "Worker " << id << " va a ejecutar una tarea." << endl;
                 wts[id].thunk();
                 wts[id].thunk = nullptr;
             }
@@ -46,6 +49,8 @@ void ThreadPool::dispatcher() {
         {
             lock_guard<mutex> lock(queueLock);
             if (!taskQueue.empty()) {
+                cout << "Dispatcher tomó una tarea de la cola." << endl;
+
                 task = taskQueue.front();
                 taskQueue.pop();
             } else {
@@ -60,6 +65,8 @@ void ThreadPool::dispatcher() {
                 if (wts[i].available) {
                     wts[i].thunk = task;
                     wts[i].available = false;
+                    cout << "Dispatcher asignó tarea al worker " << i << "." << endl;
+
                     wts[i].ready.signal();
                     assigned = true;
                     break;
