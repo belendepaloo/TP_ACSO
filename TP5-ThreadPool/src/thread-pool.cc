4a2c88b36927527958da41b54019f96efefb4900
belendepalo <104471058+belendepalo@users.noreply.github.com>
2025-06-19 11:05:32 -0300

    a

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -1,33 +1,26 @@
-/**
- * File: thread-pool.cc
- * -------------------
- * Presents the implementation of the ThreadPool class.
- */
-
 #include "thread-pool.h"
 using namespace std;
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false), availableWorkers(numThreads) {
-    // Initialize workers
+ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), availableWorkers(numThreads) {
     for (size_t i = 0; i < numThreads; ++i) {
         wts[i].id = i;
         wts[i].available = true;
-        wts[i].ts = thread([this, i] { worker(i); });
+        wts[i].ts = thread(&ThreadPool::worker, this, i);
     }
-    
-    // Start dispatcher thread
-    dt = thread([this] { dispatcher(); });
+    dt = thread(&ThreadPool::dispatcher, this);
 }
 
 void ThreadPool::schedule(const function<void(void)>& thunk) {
-    lock_guard<mutex> lock(queueLock);
-    tasks.push(thunk);
-    pendingTasks++;
+    {
+        lock_guard<mutex> lock(queueLock);
+        tasks.push(thunk);
+        pendingTasks++;
+    }
     queueCV.notify_one();
 }
 
 void ThreadPool::wait() {
-    unique_lock<mutex> lock(queueLock);
+    unique_lock<mutex> lock(waitLock);
     waitCV.wait(lock, [this] { return pendingTasks == 0; });
 }
 
@@ -35,48 +28,36 @@ ThreadPool::~ThreadPool() {
     {
         lock_guard<mutex> lock(queueLock);
         done = true;
-        queueCV.notify_all();
     }
+    queueCV.notify_all();
+    availableWorkers.signal(); // Ensure dispatcher can exit
 
-    // Notify all workers to exit
     for (auto& wt : wts) {
-        wt.semaphore.signal();
+        wt.semaphore.signal(); // Wake up workers to exit
     }
 
-    // Join all worker threads
     for (auto& wt : wts) {
-        if (wt.ts.joinable()) {
-            wt.ts.join();
-        }
+        if (wt.ts.joinable()) wt.ts.join();
     }
 
-    // Join dispatcher thread
-    if (dt.joinable()) {
-        dt.join();
-    }
+    if (dt.joinable()) dt.join();
 }
 
 void ThreadPool::worker(int id) {
     while (true) {
-        wts[id].semaphore.wait(); // Wait for work
+        wts[id].semaphore.wait();
         
-        // Check if we should exit
         if (done) break;
         
-        // Execute the task
         wts[id].thunk();
         
         {
             lock_guard<mutex> lock(queueLock);
             wts[id].available = true;
-            pendingTasks--;
-            
-            // Notify wait() if all tasks are done
-            if (pendingTasks == 0) {
+            if (--pendingTasks == 0) {
+                lock_guard<mutex> waitLockGuard(waitLock);
                 waitCV.notify_all();
             }
-            
-            // Notify dispatcher that a worker is available
             availableWorkers.signal();
         }
     }
@@ -84,29 +65,21 @@ void ThreadPool::worker(int id) {
 
 void ThreadPool::dispatcher() {
     while (true) {
-        function<void(void)> task;
+        unique_lock<mutex> lock(queueLock);
+        queueCV.wait(lock, [this] { return !tasks.empty() || done; });
         
-        {
-            unique_lock<mutex> lock(queueLock);
-            queueCV.wait(lock, [this] { return !tasks.empty() || done; });
-            
-            // Check if we should exit
-            if (done && tasks.empty()) break;
-            if (tasks.empty()) continue;
-            
-            // Wait for an available worker
-            availableWorkers.wait();
-            
-            // Find an available worker
-            for (auto& wt : wts) {
-                if (wt.available) {
-                    task = tasks.front();
-                    tasks.pop();
-                    wt.thunk = task;
-                    wt.available = false;
-                    wt.semaphore.signal();
-                    break;
-                }
+        if (done && tasks.empty()) break;
+        if (tasks.empty()) continue;
+        
+        availableWorkers.wait();
+        
+        for (auto& wt : wts) {
+            if (wt.available) {
+                wt.thunk = move(tasks.front());
+                tasks.pop();
+                wt.available = false;
+                wt.semaphore.signal();
+                break;
             }
         }
     }
